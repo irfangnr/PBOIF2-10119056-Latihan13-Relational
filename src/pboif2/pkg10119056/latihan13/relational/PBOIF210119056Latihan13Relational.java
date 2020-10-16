607943741478357c1e354072a754f84a00dfe0d8
Irfan <>
2020-10-16 19:42:32 +0700

    upload project

---
+++ src/pboif2/pkg10119056/latihan13/relational/PBOIF210119056Latihan13Relational.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119056.latihan13.relational;
+
+/**
+ *
+ * @author ACER
+ * NAMA     : Irfan Ginanjar
+ * KELAS    : PBOIF2
+ * NIM      : 10119056
+ * Deskripsi Program: Program ini berisi program untuk menampilkan relasional
+ */
+
+public class PBOIF210119056Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b =" + (bil1 == bil2));
+        System.out.println("a != b =" + (bil1 != bil2));
+        System.out.println("a > b =" + (bil1 > bil2));
+        System.out.println("a < b =" + (bil1 < bil2));
+        System.out.println("b >= a =" + (bil2 >= bil1));
+        System.out.println("b <= a =" + (bil2 <= bil1));
+        
+        
+    }
+    
+}
